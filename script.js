63bebbf95a9b0028351f2e57637a146c57cb0da0
sidg75 <siddharth.gandhi@gmail.com>
2024-10-07 22:57:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const formData = new FormData();
         formData.append('images', imageFile);
         formData.append('organs', 'leaf');  // you can change to 'flower', 'fruit', etc.
+
+
+formData.append('modifiers', JSON.stringify(["crops_fast", "similar_images"]));
+formData.append('plant_language', 'en');
+formData.append('plant_details', JSON.stringify([
+    "common_names", 
+    "url", 
+    "name_authority", 
+    "wiki_description", 
+    "taxonomy", 
+    "synonyms"
+]));
+
+               
         
         fetch('https://api.plant.id/v2/identify', {
             method: 'POST',
@@ -46,31 +60,63 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => console.error('Error:', error));
     });
 
-    function displayPlantDetails(details) {
-        const resultSection = document.getElementById('result-section');
-        resultSection.classList.remove('hidden');
+ 
+function displayPlantDetails(details) {
+    const resultSection = document.getElementById('result-section');
+    resultSection.classList.remove('hidden');
 
-        const tbody = document.querySelector('#plant-details tbody');
-        tbody.innerHTML = `
-            <tr>
-                <td>Name</td>
-                <td>${details.plant_name}</td>
-            </tr>
-            <tr>
-                <td>Scientific Name</td>
-                <td>${details.plant_details.scientific_name}</td>
-            </tr>
-            <tr>
-                <td>Family</td>
-                <td>${details.plant_details.family}</td>
-            </tr>
-            <tr>
-                <td>Genus</td>
-                <td>${details.plant_details.genus}</td>
-            </tr>
-        `;
-    }
+    const tbody = document.querySelector('#plant-details tbody');
+    tbody.innerHTML = `
+        <tr>
+            <td>Name</td>
+            <td>${details.plant_name}</td>
+        </tr>
+        <tr>
+            <td>Scientific Name</td>
+            <td>${details.plant_details.scientific_name}</td>
+        </tr>
+        <tr>
+            <td>Common Names</td>
+            <td>${details.plant_details.common_names ? details.plant_details.common_names.join(', ') : 'N/A'}</td>
+        </tr>
+        <tr>
+            <td>Name Authority</td>
+            <td>${details.plant_details.name_authority || 'N/A'}</td>
+        </tr>
+        <tr>
+            <td>Wiki Description</td>
+            <td>${details.plant_details.wiki_description?.value || 'N/A'}</td>
+        </tr>
+        <tr>
+            <td>Family</td>
+            <td>${details.plant_details.family}</td>
+        </tr>
+        <tr>
+            <td>Genus</td>
+            <td>${details.plant_details.genus}</td>
+        </tr>
+        <tr>
+            <td>Taxonomy</td>
+            <td>${details.plant_details.taxonomy 
+                ? `Class: ${details.plant_details.taxonomy.class}, Order: ${details.plant_details.taxonomy.order}, Family: ${details.plant_details.taxonomy.family}`
+                : 'N/A'}
+            </td>
+        </tr>
+        <tr>
+            <td>Synonyms</td>
+            <td>${details.plant_details.synonyms ? details.plant_details.synonyms.join(', ') : 'N/A'}</td>
+        </tr>
+        <tr>
+            <td>Plant URL</td>
+            <td><a href="${details.plant_details.url}" target="_blank">More Info</a></td>
+        </tr>
+    `;
+}
 
+
+
+
+    
     function displaySimilarPlants(similarImages) {
         const similarPlantsDiv = document.getElementById('similar-plants');
         similarPlantsDiv.innerHTML = '';
